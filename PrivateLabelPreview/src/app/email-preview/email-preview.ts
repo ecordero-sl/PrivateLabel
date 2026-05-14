3503ad229fd224863d56224783ef63d92609251b
Edward Cordero <edward.cordero@sharperlending.com>
2026-05-14 15:05:06 -0400

    remove unused component and properties

--- PrivateLabelPreview/src/app/email-preview/email-preview.ts
+++ PrivateLabelPreview/src/app/email-preview/email-preview.ts
@@ -12,20 +12,9 @@ export class EmailPreview {
   placeholder = input.required<IPrivateLabel>()
   activePreviewConfig = input<IPrivateLabel>()
   
-  isDarkTheme = signal<boolean>(false);
+  private isDarkTheme = signal<boolean>(false);
   
-  selectedPreview = computed<IPrivateLabel>(() => this.activePreviewConfig() ?? this.placeholder())
+  protected selectedPreview = computed<IPrivateLabel>(() => this.activePreviewConfig() ?? this.placeholder())
 
   plTheme = computed(() => (this.isDarkTheme() ? '#292929' : 'white'));
-  plBgColor = computed(() => this.selectedPreview().email.backgroundColor);
-  plColor = computed(() => this.selectedPreview().email.color);
-  plEmailLogo = computed(() => this.selectedPreview().email.logoFile);
-  plTopStyle = computed(
-    () =>
-      `background-color:${this.plBgColor()} !important;color:${this.plColor()} !important;font-weight:600;width:700px;min-height:80px;border-top-left-radius:10px;border-top-right-radius:10px;`
-  );
-  plBottomStyle = computed(
-    () =>
-      `background-color:${this.plBgColor()} !important;color:${this.plColor()} !important;font-weight:600;width:700px;min-height:96px;border-bottom-left-radius:10px;border-bottom-right-radius:10px`
-  );
 }
